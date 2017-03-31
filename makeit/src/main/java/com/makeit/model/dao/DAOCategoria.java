caeea23ecfccf65c9014712cc2cf657a128e0feb
Razon Miah <x6698720q@gmail.com>
2017-03-31 21:59:38 +0200

    Version 1.0 Data acces object class Categoria added

---
+++ makeit/src/main/java/com/makeit/model/dao/DAOCategoria.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makeit.model.dao;
+
+import com.makeit.model.bd.BD;
+import com.makeit.model.classes.Categoria;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author razomiah
+ */
+public class DAOCategoria {
+
+    /**
+     * Funcion per inserir Categories a la base de dades.
+     *
+     * @param categoria
+     * @return devuelve un true si se ha introducido correctamente, false en el
+     * caso contrario
+     */
+    public boolean insertCategoria(Categoria categoria) throws SQLException {
+        boolean recordOk = true;
+        PreparedStatement stmt = null;
+        try {
+            String sql = "INSERT INTO categoria(nombre) VALUES(?)";
+            stmt = conexio().prepareStatement(sql);
+            stmt.setString(1, categoria.getNombre());
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException e) {
+            recordOk = false;
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ignore) {
+                }
+            }
+        }
+        return recordOk;
+    }
+
+    /**
+     * Funcion para obtener todas las categorias registradas en la bd
+     *
+     * @return devuelve una lista de Categoria
+     */
+    public ArrayList<Categoria> getAllCategorias() {
+        ArrayList<Categoria> resultat = new ArrayList();
+        PreparedStatement stmt=null;
+        try {
+            String sql = "SELECT * FROM categoria";
+            stmt = conexio().prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                resultat.add(new Categoria(
+                        rs.getInt("id"),
+                        rs.getString("nombre")
+                ));
+            }
+        } catch (SQLException e) {
+            resultat = null;
+             if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ignore) {
+                }
+            }
+        }
+        return resultat;
+    }
+
+    /**
+     * Funcion para obtener la conexion a la bd
+     *
+     * @return
+     * @throws SQLException
+     */
+    private Connection conexio() throws SQLException {
+        return BD.getConnexio();
+    }
+}
