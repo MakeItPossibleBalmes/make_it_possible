a71a57f223691198246e6d22399b0ca8b6a8fe49
Razon Miah <x6698720q@gmail.com>
2017-03-31 21:10:09 +0200

    Version 1.0 BD class added

---
+++ makeit/src/main/java/com/makeit/model/bd/BD.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makeit.model.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author razomiah
+ */
+public class BD {
+
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/javaweb";
+    private static final String ID_DB = "root";
+    private static final String PW_DB = "";
+    private static Connection CONNECTION = null;
+
+    public static Connection getConnexio() throws SQLException {
+        if (CONNECTION == null) {
+            new BD(DATABASE_URL, ID_DB, PW_DB);
+        }
+        return CONNECTION;
+    }
+
+    private BD(String url, String id, String pw) throws SQLException {
+        connectarBBDD(url, id, pw);
+    }
+
+    private void connectarBBDD(String url, String id, String pw) throws SQLException {
+        CONNECTION = DriverManager.getConnection(url, id, pw);
+    }
+
+    public static void tancarConnexio() {
+        if (CONNECTION != null) {
+            try {
+                CONNECTION.close();
+            } catch (Exception ignore) {
+            }
+        }
+    }
+
+}
