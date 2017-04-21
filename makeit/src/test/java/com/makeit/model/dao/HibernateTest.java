20f72137c0f09228e0b284ab77fc97c0f6aab295
hartbold <ardevolp@gmail.com>
2017-04-21 23:30:59 +0200

    Esto no se que es lo que es xd

--- makeit/src/test/java/com/makeit/model/dao/HibernateTest.java
+++ makeit/src/test/java/com/makeit/model/dao/HibernateTest.java
@@ -7,20 +7,19 @@ import com.makeit.model.POJO.Usuario;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Usuario usu=new Usuario("email@email.com","123123","blabla","blabla","Fff","hhh");
-		Tema tema=new Tema();
-		DAOUsuario daou=new DAOUsuario();
-	/* {
-			daou.insertUsuario(usu);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-		List<Usuario> usuarios=daou.getAllUsuario();
-		
-		System.out.println(usuarios);
+//		Usuario usu=new Usuario("email@email.com","123123","blabla","blabla","Fff","hhh");
+//		Tema tema=new Tema();
+//		DAOUsuario daou=new DAOUsuario();
+//	/* {
+//			daou.insertUsuario(usu);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}*/
+//		
+//		List<Usuario> usuarios=daou.getAllUsuario();
+//		
+//		System.out.println(usuarios);
 
 	}
 
