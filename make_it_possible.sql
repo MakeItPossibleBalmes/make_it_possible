dea38622ed466e840e9eee637bfb9c06837f0df3
razondpro <x6698720q@gmail.com>
2017-04-21 13:05:40 +0200

    datases tables update

--- make_it_possible.sql
+++ make_it_possible.sql
@@ -1,186 +1,135 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 11, 2017 at 06:21 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema make_it_possible
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `make_it_possible` ;
+
+-- -----------------------------------------------------
+-- Schema make_it_possible
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `make_it_possible` DEFAULT CHARACTER SET utf8 ;
+USE `make_it_possible` ;
+
+-- -----------------------------------------------------
+-- Table `make_it_possible`.`categorias`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `make_it_possible`.`categorias` ;
+
+CREATE TABLE IF NOT EXISTS `make_it_possible`.`categorias` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nombre` (`nombre` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `make_it_possible`.`usuarios`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `make_it_possible`.`usuarios` ;
 
---
--- Database: `make_it_possible`
---
-CREATE DATABASE IF NOT EXISTS `make_it_possible` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `make_it_possible`;
+CREATE TABLE IF NOT EXISTS `make_it_possible`.`usuarios` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `dni` VARCHAR(9) NULL DEFAULT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `nombre` VARCHAR(75) NOT NULL,
+  `primer_apellido` VARCHAR(75) NOT NULL,
+  `segundo_apellido` VARCHAR(75) NULL DEFAULT NULL,
+  `codigo_postal` INT(5) NULL DEFAULT NULL,
+  `ciudad` VARCHAR(65) NOT NULL,
+  `pais` VARCHAR(65) NOT NULL,
+  `is_admin` TINYINT(1) NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC),
+  UNIQUE INDEX `dni_UNIQUE` (`dni` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `categorias`
---
+-- -----------------------------------------------------
+-- Table `make_it_possible`.`temas`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `make_it_possible`.`temas` ;
 
-CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `make_it_possible`.`temas` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(255) NOT NULL,
+  `cuerpo` TEXT NOT NULL,
+  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `usuario_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`, `usuario_id`),
+  INDEX `fk_tema_usuario1_idx` (`usuario_id` ASC),
+  CONSTRAINT `fk_tema_usuario1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `make_it_possible`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `temas`
---
+-- -----------------------------------------------------
+-- Table `make_it_possible`.`tema_categoria`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `make_it_possible`.`tema_categoria` ;
 
-CREATE TABLE `temas` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `cuerpo` text NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `total_firmas` int(11) NOT NULL DEFAULT '0',
-  `autor_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `make_it_possible`.`tema_categoria` (
+  `tema_id` INT(11) NOT NULL,
+  `categoria_id` INT(11) NOT NULL,
+  PRIMARY KEY (`tema_id`, `categoria_id`),
+  INDEX `fk_tema_has_categoria_categoria1_idx` (`categoria_id` ASC),
+  INDEX `fk_tema_has_categoria_tema_idx` (`tema_id` ASC),
+  CONSTRAINT `fk_tema_has_categoria_categoria1`
+    FOREIGN KEY (`categoria_id`)
+    REFERENCES `make_it_possible`.`categorias` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tema_has_categoria_tema`
+    FOREIGN KEY (`tema_id`)
+    REFERENCES `make_it_possible`.`temas` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `temas_categorias`
---
+-- -----------------------------------------------------
+-- Table `make_it_possible`.`voto`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `make_it_possible`.`voto` ;
 
-CREATE TABLE `temas_categorias` (
-  `id` int(11) NOT NULL,
-  `id_tema` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `make_it_possible`.`voto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` INT(11) NOT NULL,
+  `tema_id` INT(11) NOT NULL,
+  `fecha_creacion` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`, `usuario_id`, `tema_id`),
+  INDEX `fk_usuarios_has_temas_temas1_idx` (`tema_id` ASC),
+  INDEX `fk_usuarios_has_temas_usuarios1_idx` (`usuario_id` ASC),
+  CONSTRAINT `fk_usuarios_has_temas_temas1`
+    FOREIGN KEY (`tema_id`)
+    REFERENCES `make_it_possible`.`temas` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_has_temas_usuarios1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `make_it_possible`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `nombre` varchar(75) NOT NULL,
-  `primer_apellido` varchar(75) NOT NULL,
-  `segundo_apellido` varchar(75) DEFAULT NULL,
-  `codigo_postal` int(5) NOT NULL,
-  `ciudad` varchar(65) NOT NULL,
-  `pais` varchar(65) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuarios_temas`
---
-
-CREATE TABLE `usuarios_temas` (
-  `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `tema_id` int(11) NOT NULL,
-  `fecha_firmado` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
-
---
--- Indexes for table `temas`
---
-ALTER TABLE `temas`
-  ADD PRIMARY KEY (`id`,`autor_id`),
-  ADD KEY `fk_tema_usuario1_idx` (`autor_id`);
-
---
--- Indexes for table `temas_categorias`
---
-ALTER TABLE `temas_categorias`
-  ADD PRIMARY KEY (`id`,`id_tema`,`id_categoria`),
-  ADD KEY `fk_tema_has_categoria_categoria1_idx` (`id_categoria`),
-  ADD KEY `fk_tema_has_categoria_tema_idx` (`id_tema`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `dni_UNIQUE` (`dni`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `usuarios_temas`
---
-ALTER TABLE `usuarios_temas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario_id` (`usuario_id`),
-  ADD KEY `tema_id` (`tema_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `temas`
---
-ALTER TABLE `temas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `temas_categorias`
---
-ALTER TABLE `temas_categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuarios_temas`
---
-ALTER TABLE `usuarios_temas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `temas`
---
-ALTER TABLE `temas`
-  ADD CONSTRAINT `fk_tema_usuario1` FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `temas_categorias`
---
-ALTER TABLE `temas_categorias`
-  ADD CONSTRAINT `fk_tema_has_categoria_categoria1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_tema_has_categoria_tema` FOREIGN KEY (`id_tema`) REFERENCES `temas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
