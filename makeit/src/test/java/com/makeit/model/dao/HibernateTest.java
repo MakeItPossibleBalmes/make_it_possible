05304762b4a96fd8acfd23fb8eed537344c9656a
Razon Miah <x6698720q@gmail.com>
2017-04-21 17:17:56 +0200

    Subiendo cambios

---
+++ makeit/src/test/java/com/makeit/model/dao/HibernateTest.java
@@ -0,0 +1,27 @@
+package com.makeit.model.dao;
+
+import java.util.List;
+
+import com.makeit.model.POJO.Usuario;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Usuario usu=new Usuario("email@email.com","123123","blabla","blabla","Fff","hhh");
+		Tema tema=new Tema();
+		DAOUsuario daou=new DAOUsuario();
+	/* {
+			daou.insertUsuario(usu);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		List<Usuario> usuarios=daou.getAllUsuario();
+		
+		System.out.println(usuarios);
+
+	}
+
+}
