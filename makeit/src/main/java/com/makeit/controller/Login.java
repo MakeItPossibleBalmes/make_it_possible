eb40c086fd1a56190e9537c98a188135264f60ed
Razon Miah <x6698720q@gmail.com>
2017-04-25 21:24:35 +0200

    login actualizado

--- makeit/src/main/java/com/makeit/controller/Login.java
+++ makeit/src/main/java/com/makeit/controller/Login.java
@@ -5,8 +5,16 @@
  */
 package com.makeit.controller;
 
+import com.makeit.exceptions.InvalidEmail;
+import com.makeit.exceptions.InvalidName;
+import com.makeit.exceptions.PasswordException;
+import com.makeit.model.POJO.Usuario;
+import com.makeit.model.dao.DAOUsuario;
+import com.makeit.model.util.Crypt;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -31,29 +39,42 @@ public class Login extends HttpServlet {
      */
     protected void procesarLogin(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String email = request.getParameter("email");
-        String contraseña = request.getParameter("password");
-        String recordar = request.getParameter("recordarme");
-        
-        boolean error = false;
-        
-        /*TODO:   
-            Las conexiones al final las establecemos con hibernate? 
+        String email = "";
+        try {
+            email = request.getParameter("email");
+            String psw = request.getParameter("password");
+            if (Comprobacio.isValidEmailAddress(email) && Comprobacio.isValidPassword(psw, psw)) {
+                Usuario usuario = DAOUsuario.getUsuario(email);
+                if (usuario.getPassword().equals(Crypt.encripta(psw))) {
+                    request.getSession().setAttribute("usuario", usuario);
+                    response.sendRedirect(request.getContextPath() + "/");
+                }else{
+                    request.setAttribute("error", "datos contraseña incorrecta");
+                    doGet(request, response);
+                }
+            }
+        } catch (InvalidEmail e) {
+            request.setAttribute("error", "email invalido");
+        } catch (PasswordException e) {
+            request.setAttribute("error", "contraseña invalida");
+        } catch (Exception e) {
+            request.setAttribute("error", "error extraño, contacte con el admin");
+        } finally {
+            request.setAttribute("email", email);
+            doGet(request, response);
+        }
+
+        /*TODO:
+            Las conexiones al final las establecemos con hibernate?
             Función de encriptación.
             Función para comprobar si el formato de dni que llega es correcto - aún que ya se valide en el form.
             Añadir un e-mail al usuario estaría guai.
-        */
-        
-        
+         */
         //-------------------------------
         //GET USUARIO CON EL MISMO NOMBRE
-        
         //RECOGER SU CONTRASEÑA Y COMPRARARLA A LA QUE NOS LLEGA (comprar encriptada)
-        
         //SI EXISTE --> CREAR COOKIE DE SESION
         //SI NO --> REDIRIGIR A LA PÁGINA DE INICIO CON EL MENSAJE DE ERROR.
-        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -68,7 +89,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    	RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp");
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp");
         dispatcher.forward(request, response);
     }
 
