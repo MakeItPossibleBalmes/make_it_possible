2ec6c7ee5f8856810cd5e077650d5fa83a803f6a
Razon Miah <x6698720q@gmail.com>
2017-04-26 17:58:57 +0200

    new controller for Voto

---
+++ makeit/src/main/java/com/makeit/controller/Voto.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makeit.controller;
+
+import com.makeit.model.POJO.Usuario;
+import com.makeit.model.POJO.VotoTemaUsuario;
+import com.makeit.model.dao.DAOTema;
+import com.makeit.model.dao.DAOUsuario;
+import com.makeit.model.dao.DAOVoto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author razomiah
+ */
+public class Voto extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        Usuario usuario = DAOUsuario.getUsuario(Integer.parseInt(request.getParameter("id_usuario")));
+        com.makeit.model.POJO.Tema tema = DAOTema.getTema(Integer.parseInt(request.getParameter("id_tema")));
+        try (PrintWriter out = response.getWriter()) {
+            switch (request.getParameter("accio")) {
+                case "add_voto":
+                        DAOVoto.insertVoto(usuario,tema);
+                    break;
+                case "cancelar_voto":
+                    Usuario u=DAOUsuario.getUsuario(usuario.getId());  
+                    u.getVotos().remove(u);
+                    break;
+            }
+        } catch (Exception e) {
+            //tratar excepcion
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
